1166948c7c3e84cb06b0700b3856c34db5aa9f7b
ozgunyargi <ozgun.yargi@gmail.com>
2021-01-22 16:10:28 +0300

    Most of the GUI functions are finished

--- GUI.py
+++ GUI.py
@@ -1,9 +1,47 @@
+import tkinter as tk
 from tkinter import *
-window = Tk()
-window.title("Test")
-window.geometry("400x200")
-L1 = Label(window, text="User Name")
-L1.pack( side = LEFT)
-E1 = Entry(window, bd =5)
-E1.pack(side = RIGHT)
+import string_to_txt
+
+def click():
+    entered_objective = L1Entry.get()
+    entered_constraints = L7Entry.get()
+    entered_objective_string = L5Entry.get()
+
+    string_to_txt.objective_to_txt(entered_objective)
+    string_to_txt.constraints_to_txt(entered_constraints)
+    string_to_txt.variablestr_to_txt(entered_objective_string)
+
+window = tk.Tk()
+window.title("Revised Simplex Method")
+window.configure(background="white")
+
+
+window.geometry("500x200")
+L0 = Label(window, text="", bg="white").grid(row=0, column=0, sticky=W)
+
+L1 = Label(window, text="Objective (max or min):", bg="white").grid(row=1, column=0, sticky=W)
+L1Entry = Entry (window, width=20, bg="white")
+L1Entry.grid(row=1, column=2, sticky=W)
+
+L2 = Label(window, text="", bg="white").grid(row=2, column=0, sticky=W)
+
+#L3 = Label(window, text="Objective string:", bg="white").grid(row=3, column=0, sticky=W)
+#L3Entry = Entry (window, width=20, bg="white").grid(row=3, column=2, sticky=W)
+
+#L4 = Label(window, text="", bg="white").grid(row=4, column=0, sticky=W)
+
+L5 = Label(window, text="variable list (ex: x1 x2 x3 x4 (up to x9 (lower case))):", bg="white").grid(row=5, column=0, sticky=W)
+L5Entry = Entry (window, width=20, bg="white")
+L5Entry.grid(row=5, column=2, sticky=W)
+
+L6 = Label(window, text="", bg="white").grid(row=6, column=0, sticky=W)
+
+L7 = Label(window, text="constraints:\n(ex:1*x1+3*x2<=10_-1*x2+2*x3<=5)", bg="white").grid(row=7, column=0, sticky=W)
+L7Entry = Entry (window, width=20, bg="white")
+L7Entry.grid(row=7, column=2, sticky=W)
+
+Button(window, text= "Execute", width=6, command=click).grid(row=8, column = 1)
+
+
+
 window.mainloop()
---
+++ string_to_txt.py
@@ -0,0 +1,104 @@
+def objective_to_txt(string):
+
+    myTxt = open("Objective.txt", "w+")
+
+    for i in range(1):
+
+        myTxt.write(string)
+
+
+    myTxt.close()
+
+def constraints_to_txt(string):
+
+    valTxt = open("values.txt", "w+")
+    resultTxt = open("result.txt", "w+")
+    variableTxt = open("constraint_variables.txt", "w+")
+
+    equations = string.split("_")
+
+    idenList = ["<=", ">=", "<", ">"]
+
+    identifierList = []
+
+    for equ in equations:
+
+        identifier = ""
+
+        for i in idenList:
+
+            found = equ.find(i)
+
+            if found != -1:
+
+                identifierList.append(i)
+
+                break
+
+    idencounter=0
+
+    for equation in equations:
+
+        counter = 0
+
+        sides = equation.split(identifierList[idencounter])
+
+        for side in sides:
+
+            if counter == 0:
+
+                element = side.split("*")
+
+                valstr = ""
+                variablestr =""
+
+                for val in element:
+
+                    if val.find("x") !=-1 and element.index(val) != len(element)-1:
+
+                        num = int(val[2:])
+
+                        valstr = valstr + " " + str(num)
+
+                        variablestr = variablestr + " " + val[:2]
+
+                    elif element.index(val) != len(element)-1:
+                        num = int(val)
+
+                        valstr = valstr + " " + str(num)
+
+                    else:
+                        variablestr = variablestr + " " + val
+
+                valTxt.write(valstr)
+                valTxt.write("\n")
+
+                variableTxt.write(variablestr)
+                variableTxt.write("\n")
+
+            else:
+
+                resultTxt.write(side + " " + identifierList[idencounter])
+                resultTxt.write("\n")
+
+            counter += 1
+
+        idencounter += 1
+
+    valTxt.close()
+
+    resultTxt.close()
+
+
+def variablestr_to_txt(string):
+
+    myTxt = open("variables.txt", "w+")
+
+    myTxt.write(string)
+
+    myTxt.close()
+
+
+#string = "1*x1+3*x2<=10_-1*x2+2*x3<=5"
+
+#constraints_to_txt(string)
